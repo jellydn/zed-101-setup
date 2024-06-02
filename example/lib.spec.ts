274e6499e28261deaf294235a2fc2a97eaecdce7
Huynh Duc Dung <dunghd.it@gmail.com>
2024-06-02 23:17:34 +0800

    refactor!: move test to its folder

--- example/lib.spec.ts
+++
@@ -1,8 +0,0 @@
-import { describe, expect, test } from "bun:test";
-import { calculateAge } from "./lib";
-
-describe("lib", () => {
-	test("calculate age", () => {
-		expect(calculateAge(1988)).toBe(36);
-	});
-});
--- example/lib.ts
+++ example/lib.ts
@@ -2,7 +2,11 @@
  * Calculate the age of a person based on the year of birth
  * @param bornInYear The year the person was born
  */
-export function calculateAge(bornInYear: number) {
-	const currentYear = new Date().getFullYear();
-	return currentYear - bornInYear;
+export function calculateAge(bornInYear: number): number {
+  const currentYear = new Date().getFullYear();
+  return currentYear - bornInYear;
+}
+
+export function greeter(name: string): void {
+  console.log(`Hello ${name}`);
 }
